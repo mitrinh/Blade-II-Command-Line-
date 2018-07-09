e01ebc4be803aef1d01b4947705baa6c68ba40a8
Michael T <32150483+mitrinh@users.noreply.github.com>
2018-07-08 23:27:38 -0700

    Add files via upload

--- card.h
+++ card.h
@@ -11,18 +11,26 @@ using namespace std;
 
 class card {
 private:
+    bool bolted; // false = not bolted, true = bolted
+    unsigned int cardType; // 0 = regCard, 1 = bolt, 2 = mirror, 3 = blast, 4 = force
     unsigned int value;
     string name;
 public:
     card();
     ~card();
     unsigned int getValue() const;
-    void setValue(unsigned int thisValue);
+    void setValue(unsigned int);
     string getName() const;
-    void setName(string thisName);
+    void setName(string);
+    unsigned int getCardType() const;
+    void setCardType(unsigned int);
+    bool getBolted() const;
+    void setBolted(bool);
 };
 
 card::card() {
+    bolted = false;
+    cardType = 0;
     value = 0;
     name = "empty";
 }
@@ -36,5 +44,13 @@ void card::setValue(unsigned int thisValue) { value = thisValue; }
 string card::getName() const { return name; }
 
 void card::setName(string thisName){ name = std::move(thisName); }
+
+unsigned int card::getCardType() const { return cardType; }
+
+void card::setCardType(unsigned int cardType_) { cardType = cardType_; }
+
+bool card::getBolted() const { return bolted; }
+
+void card::setBolted(bool bolted_) { bolted = bolted_; }
 
 #endif //BLADE_CARD_H
