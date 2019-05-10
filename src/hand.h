03d94d5edf32fcde208b4c256350287d96d5e657
Michael Trinh <mptrinh@cpp.edu>
2019-05-09 20:18:35 -0700

    final draft

--- src/hand.h
+++ src/hand.h
@@ -1,6 +1,28 @@
 //
-// Created by PC on 7/3/2018.
+//    Name:         Trinh, Michael
+//    Project:      Blade
+//    Date Started: 06/15/18
+//    Date Finished:08/21/18
 //
+//    Description:      A card game based on the minigame in the "Trails" video game franchise.
+//                  -The game is set up with a deck of 44 cards and is shared between 2 players holding 10 cards taken
+//                      10 cards from the deck.
+//                  -The cards have their own value or score whenever they are played.
+//                  -At the start of the game, both players play a card from the top of the deck and whoever's score is
+//                      lower goes first.
+//                  -The win condition is to "checkmate" the other  player so that their score is lower than yours after
+//                      they play a card.
+//                  -Whenever both scores are equal,clear all played cards and current scores then start the game again.
+//                  -There are 2 types of cards: regular cards of values 1-7, and special cards which have a base value
+//                      of 1 when played from the deck or an effect depending on the card when played from the hand.
+//
+//                  -There are 4 different special cards: blast, bolt, force, and mirror.
+//                      -blast lets you choose a random card from your opponent's hand and discard, then lets you play
+//                          again.
+//                      -bolt negates the opponent's last played card's value if it is regular or force. Cards that have
+//                          been bolted can be restored by a regular "1" card.
+//                      -force doubles the score of your played cards.
+//                      -mirror swaps the opponent's played cards and score with yours.
 
 #ifndef BLADE_HAND_H
 #define BLADE_HAND_H
@@ -8,7 +30,6 @@
 #include "card.h"
 #include <algorithm>
 #include <chrono>
-#include <iostream>
 #include <memory>
 #include <random>
 #include <stack>
@@ -29,7 +50,8 @@ public:
     void setRegularCards(unsigned int);
     void setSpecialCards(unsigned int);
     void createHand(stack<card>&);
-    void printHand() const;
+    string printHand();
+    string printHidden();
     void shuffleHand();
     void sortHand();
 };
@@ -46,12 +68,18 @@ unsigned int hand::getRegularCards() const { return regularCards; } // end getRe
 unsigned int hand::getSpecialCards() const { return specialCards; } // end getSpecialCards
 
 // output hand
-void hand::printHand() const {
-    cout << "Regular Cards: " << getRegularCards() << '\t';
-    cout << "Special Cards: " << getSpecialCards() << endl;
-    for (auto &i : hand_) cout << i.getName() << " ";
-    cout << endl;
+string hand::printHand() {
+    string output;
+    for (auto &card : hand_) output += "[" + card.getName() + "] ";
+    return output;
 } // end printHand
+
+// output hidden version of hand
+string hand::printHidden() {
+    string output;
+    for (auto &card : hand_) output += "[?] ";
+    return output;
+} // end printHidden
 
 void hand::setRegularCards(unsigned int regCards){ regularCards = regCards; } // end setRegularCards
 
@@ -65,12 +93,12 @@ bool sortByName(const card &lhs, const card &rhs) { return lhs.getName() < rhs.g
 
 // give 10 cards to both hands from the shared deck at start of duel
 void hand::createHand(stack<card> &deck) {
-    unique_ptr<card> temp(&deck.top());
+    card temp = deck.top();
     for(int i = 0; i < START_HAND_QUANTITY; i++) {
-        *temp = deck.top();
-        if (isRegularCard(*temp)) setRegularCards(1+getRegularCards());
+        temp = deck.top();
+        if (isRegularCard(temp)) setRegularCards(1+getRegularCards());
         else setSpecialCards(1+getSpecialCards());
-        hand_.push_back(*temp);
+        hand_.push_back(temp);
         deck.pop();
     }
 } // end createHand
@@ -94,4 +122,5 @@ void hand::sortHand() {
     regHand->insert(regHand->end(),specialHand->begin(),specialHand->end());
     hand_.swap(*regHand);
 } // end sortHand
+
 #endif //BLADE_HAND_H
